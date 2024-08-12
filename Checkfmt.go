808d295fff1d20bf43b1c19a2a72fbf451aa5d05
roim10 <164913051+roim10@users.noreply.github.com>
2024-08-12 22:43:17 +0300

    Checkfmt.go

--- Checkfmt.go
+++ Checkfmt.go
@@ -9,13 +9,15 @@ func main() {
 	var MyList MyListNumbers
 	fmt.Println(MyList.count())
 }
-
+// создаем структуру с именем MyListNumbers
 type MyListNumbers struct {
+// создаем две переменных типа string с именами i и j 
 	i string
 	j string
 }
-
+// создаем метод count который возращает тип строк, а принимает переменную с типом структуры MyListNumbers
 func (MyList MyListNumbers) count() string {
+	
 	MyList.i = "1"
 	MyList.j = "2"
 	return fmt.Sprintf("enter numbers %s %s", MyList.i, MyList.j)
